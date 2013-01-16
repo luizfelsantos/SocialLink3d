63d8bd252cb3df77bd17abcbeb6d6b5654a15b4e
Luiz Felipe <luizfelipe.sp95@hotmail.com>
2013-01-16 12:42:30 -0200

    Atualizando diretório principal

---
+++ README.md
@@ -0,0 +1,3 @@
+##Social Link 3D
+
+by [@felipe_web_dev](https://twitter.com/felipe_web_dev)
---
+++ js/script.js
@@ -0,0 +1,38 @@
+/**
+ *	
+ * Copyright (C) 2013 Luiz Felipe, @felipe_web_dev
+ *
+ */
+
+(function(){
+
+	var item = document.getElementsByClassName("content-social"),
+		numberItens = item.length,
+		contentSocial = document.getElementsByClassName("social"),
+		sizeItem = item[0].getBoundingClientRect().width,
+		vendors = ["Webkit","Moz","O","ms"];
+
+
+	document.addEventListener("mousemove",rotate,false);
+
+	function rotate(event){
+		var x = event.pageX,
+			y = event.pageY;
+
+		if(y >= item[0].offsetTop - 100){
+			for(var i = 0; i < numberItens; i++){
+				if(x >= item[i].offsetLeft-30 && x <= item[i].offsetLeft + sizeItem + 30){
+					for(var j = 0; j < vendors.length; j++){
+						item[i].style[vendors[j]+"Transform"] = "perspective(800px) rotateY(" + (x-item[i].offsetLeft+30) + "deg)";
+
+						contentSocial[i].style[vendors[j]+"Transform"] = "perspective(800px) rotateY(" + (x-item[i].offsetLeft+210) + "deg)";
+					}
+				}
+				else{
+					item[i].setAttribute("style","");
+					contentSocial[i].setAttribute("style","");
+				}
+			}
+		}
+	}
+})();
