37fdd01cb3b4fe02cdfae33aa2ecdc429bc14c2e
Luiz Felipe <luizfelipe.sp95@hotmail.com>
2013-01-17 19:42:49 -0200

    Renomeando variáveis

--- js/script.js
+++ js/script.js
@@ -1,33 +1,32 @@
 /**
  * Social Link 3d v1.0
  * https://github.com/felipeluiz/SocialLink3d
- * MIT LIcensed
+ * MIT Licensed
  *
  * Copyright (C) 2013 Luiz Felipe, @felipe_web_dev
  */
 
 (function(){
 
-	var elementFront  = document.getElementsByClassName("front"),
-		numberItens = elementFront .length,
-		elementBack  = document.getElementsByClassName("back"),
+	var elementFront = document.getElementsByClassName("front"),
+		numberItens = elementFront.length,
+		elementBack = document.getElementsByClassName("back"),
 		sizeItem = elementFront[0].offsetWidth,
-		vendors = ["Webkit","Moz","O","ms"];
-
+		vendors = ["Webkit","Moz","O","ms"],
+		vendorsLen = vendors.length;
 
 	document.addEventListener("mousemove",rotate,false);
 
 	function rotate(event){
-		var x = event.pageX,
-			y = event.pageY;
+		var mouseX = event.pageX;
 
-		if(y >= elementFront[0].offsetTop - 100){
+		if(event.pageY >= elementFront[0].offsetTop - 100){
 			for(var i = 0; i < numberItens; i++){
-				if(x >= elementFront[i].offsetLeft-30 && x <= elementFront[i].offsetLeft + sizeItem + 30){
-					for(var j = 0; j < vendors.length; j++){
-						elementFront[i].style[vendors[j]+"Transform"] = "perspective(800px) rotateY(" + (x-elementFront[i].offsetLeft+30) + "deg)";
+				if(mouseX >= elementFront[i].offsetLeft - 30 && mouseX <= elementFront[i].offsetLeft + sizeItem + 30){
+					for(var j = 0; j < vendorsLen; j++){
+						elementFront[i].style[vendors[j]+"Transform"] = "perspective(800px) rotateY(" + (mouseX - elementFront[i].offsetLeft + 30) + "deg)";
 
-						elementBack[i].style[vendors[j]+"Transform"] = "perspective(800px) rotateY(" + (x-elementFront[i].offsetLeft+210) + "deg)";
+						elementBack[i].style[vendors[j]+"Transform"] = "perspective(800px) rotateY(" + (mouseX - elementFront[i].offsetLeft + 210) + "deg)";
 					}
 				}
 				else{
