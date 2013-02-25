07cd9d1f052f1213211374313d8dae6909c94dd6
Luiz Felipe <luizfelipe.sp95@hotmail.com>
2013-02-25 10:43:35 -0300

    Corrigindo sintaxe e removendo botão

--- js/script.js
+++ js/script.js
@@ -8,14 +8,14 @@
 
 (function(){
 
-	var elementFront = document.getElementsByClassName("front"),
+	var elementFront = document.getElementsByClassName( 'front' ),
 		numberItens = elementFront.length,
-		elementBack = document.getElementsByClassName("back"),
+		elementBack = document.getElementsByClassName( 'back' ),
 		sizeItem = elementFront[0].offsetWidth,
-		vendors = ["Webkit","Moz","O","ms"],
+		vendors = [ 'Webkit', 'Moz', 'O', 'ms' ],
 		vendorsLen = vendors.length,
-		spinSpace = (360 - sizeItem)/2,
-		buttonLock = [].slice.call(document.getElementsByClassName("setRotate"));
+		spinSpace = ( 360 - sizeItem )/2,
+		buttonLock = [].slice.call(document.getElementsByClassName( 'setRotate' ));
 
 	// variable by hakim.se
 	var supports3DTransforms =  'WebkitPerspective' in document.body.style ||
@@ -26,63 +26,65 @@
 
 	if(supports3DTransforms){
 
-		document.addEventListener("mousemove", rotate, false);
-		document.addEventListener("click", OnClick, false);
+		document.addEventListener( 'mousemove', rotate, false );
+		buttonLock.forEach(function( element, i ){
+			element.addEventListener( 'click', OnClick, false );
+		});
 
 		// estiliza o elemento com base na posição do mouse
-		function rotate(event){
+		function rotate( event ){
 			var mouseX = event.pageX;
 
-			if(event.pageY >= elementFront[0].offsetTop - 100){
-				for(var i = 0; i < numberItens; i++){
+			if( event.pageY >= elementFront[0].offsetTop - 100 ){
+				for( var i = 0; i < numberItens; i++ ){
 
-					if(mouseX >= (elementFront[i].offsetLeft - spinSpace) &&
-						mouseX <= (elementFront[i].offsetLeft + sizeItem + spinSpace) &&
-						buttonLock[i].getAttribute("data-rotation") == "active"){
+					if(mouseX >= ( elementFront[i].offsetLeft - spinSpace ) &&
+						mouseX <= ( elementFront[i].offsetLeft + sizeItem + spinSpace ) &&
+						buttonLock[i].getAttribute( 'data-rotation' ) == 'active' ){
 
-						var posFront = (mouseX - elementFront[i].offsetLeft + spinSpace),
-							posBack = (mouseX - elementFront[i].offsetLeft + (180 + spinSpace));
+						var posFront = ( mouseX - elementFront[i].offsetLeft + spinSpace ),
+							posBack = ( mouseX - elementFront[i].offsetLeft + ( 180 + spinSpace ));
 
 						for(var j = 0; j < vendorsLen; j++){
-							elementFront[i].style[vendors[j]+"Transform"] = "perspective(800px) rotateY(" + posFront + "deg)";
-							elementBack[i].style[vendors[j]+"Transform"] = "perspective(800px) rotateY(" + posBack + "deg)";
+							elementFront[i].style[vendors[j] + 'Transform'] = 'perspective(800px) rotateY(' + posFront + 'deg)';
+							elementBack[i].style[vendors[j] + 'Transform'] = 'perspective(800px) rotateY(' + posBack + 'deg)';
 						}
 					}
 					else{
-						elementFront[i].setAttribute("style","");
-						elementBack[i].setAttribute("style","");
+						elementFront[i].setAttribute( 'style' , '');
+						elementBack[i].setAttribute( 'style' , '');
 					}
 				}
 			}
 		}
 
-		function OnClick(event){
+		function OnClick( event ){
 			var element = event.target,
-				index = buttonLock.indexOf(element);
+				index = buttonLock.indexOf( element );
 
-			if(buttonLock[index].getAttribute("data-rotation") == "active"){
-				applyStyle(element, index, "Unlock");
+			if(buttonLock[index].getAttribute( 'data-rotation' ) == 'active'){
+				applyStyle( element, index, 'Unlock' );
 			}
 			else{
-				applyStyle(element, index, "Lock");
+				applyStyle( element, index, 'Lock' );
 			}		
 		}
 
 		// avalia o atributo data dos botões de travamento decidindo se
 		// adiociona ou subtrai ao estilos, travando ou destravando a rotação
-		function applyStyle(element, i, text){
-			var state = buttonLock[i].getAttribute("data-rotation") == "active" ? "deactive" : "active";
-			buttonLock[i].setAttribute("data-rotation", state);
+		function applyStyle( element, i, text ){
+			var state = buttonLock[i].getAttribute( 'data-rotation' ) == 'active' ? 'deactive' : 'active';
+			buttonLock[i].setAttribute( 'data-rotation', state );
 			
 			element.innerHTML = text;
 
-			elementFront[i].classList.toggle("close");
-			elementBack[i].classList.toggle("open");
+			elementFront[i].classList.toggle( 'close' );
+			elementBack[i].classList.toggle( 'open' );
 		}
 	}
 	else{
-		buttonLock.forEach(function(element, i){
-			element.parentNode.removeChild(element);
+		buttonLock.forEach(function( element, i ){
+			element.parentNode.removeChild( element );
 		});
 	}
 })();
